54f93f517a9a1baeb886bfb7ba9c72b456967e1b
bharath8858 <105233330+bharath8858@users.noreply.github.com>
2023-09-21 18:15:18 +0530

    commit

--- src/main/java/PomClasses/AddNewCoursePage.java
+++ src/main/java/PomClasses/AddNewCoursePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import genericLibraries.WebDriverUtility;
-
+//entra
 public class AddNewCoursePage {
 	
 	//WebDriverUtility web= new WebDriverUtility();
